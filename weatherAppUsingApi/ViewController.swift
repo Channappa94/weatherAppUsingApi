d51d357bab97c009d4dff726a18221ceb3732b90
Phani_Kiran <pret321@gmail.com>
2019-08-05 21:31:05 -0500

    Checked if the user enters nil and wrong city.

--- weatherAppUsingApi/ViewController.swift
+++ weatherAppUsingApi/ViewController.swift
@@ -26,37 +26,45 @@ class ViewController: UIViewController {
         if cityTemperature.text?.isEmpty ?? true{
             weatherDetail.text = "Please enter the city name"
         }else{
-        let newCity = newTemp.replacingOccurrences(of: " ", with: "%20")
-        
-        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + newCity + ",us&appid=fc8ad9c4ee937c37f6283902489ed2a4") {
-            print(url)
-            let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-                if error == nil{
-                    if let unWrappedData = data {
-                        do{
-                            let jsonResult = try JSONSerialization.jsonObject(with: unWrappedData, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-
-                            print(jsonResult!["base"] as? String)
-                            let weather = jsonResult?["weather"] as? NSArray
-                      
-                            let weatherItem = weather?[0] as? NSDictionary
-                            let description = weatherItem!["description"] as! String
-                           
-                            DispatchQueue.main.async{
-                                self.weatherDetail.text = description
+            let newCity = newTemp.replacingOccurrences(of: " ", with: "%20")
+            
+            if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + newCity + ",us&appid=fc8ad9c4ee937c37f6283902489ed2a4") {
+                print(url)
+                let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                    if error == nil{
+                        if let unWrappedData = data {
+                            let dataString = String(data: unWrappedData, encoding: .utf8)
+                            if dataString!.contains("city not found"){
+                                DispatchQueue.main.async {
+                                    self.weatherDetail.text = "Not found"
+                                }
+                            }else{
+                                do{
+                                    var jsonResult = try JSONSerialization.jsonObject(with: unWrappedData, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+                                    
+                                    print(jsonResult!["base"] as? String)
+                                    
+                                    let weather = jsonResult?["weather"] as? NSArray
+                                    
+                                    let weatherItem = weather?[0] as? NSDictionary
+                                    let description = weatherItem!["description"] as! String
+                                    
+                                    DispatchQueue.main.async{
+                                        self.weatherDetail.text = description
+                                    }
+                                    
+                                }catch{
+                                    print("Error while fetching API")
+                                }
                             }
-                            
-                        }catch{
-                            print("Error while fetching API")
                         }
                     }
+                    
                 }
+                task.resume()
+                
                 
             }
-            task.resume()
-            
-            
-        }
         }
         
         
@@ -67,8 +75,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "rain")!)
         print(text)
-
-       
+        
+        
     }
     
     
